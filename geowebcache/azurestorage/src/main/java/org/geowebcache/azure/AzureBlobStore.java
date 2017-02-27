dc07e557a47754eb6fbbd9a32fbd6988ea0e9384
Jonathan Collinge <jonathancollinge@live.com>
2017-02-27 12:16:29 +0000

    Added initial project config

---
+++ geowebcache/azurestorage/src/main/java/org/geowebcache/azure/AzureBlobStore.java
@@ -0,0 +1,10 @@
+package org.geowebcache.azure;
+
+import org.geowebcache.storage.BlobStore;
+
+/**
+ * Created by jocollin on 27/02/2017.
+ */
+public class AzureBlobStore implements BlobStore {
+
+}
